acb9515321bd1c9d6bde6355f6948fa555e9a1bb
elikoschwitz <e.koschwitz@googmail.com>
2022-12-16 17:10:46 +0100

    Init: implement delete, get, post as a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+
+type Todo= {
+  id : string;
+  description : string;
+  status : string;
+};
+
+type postTodo= {
+    description : string;
+    status : string;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // get endpunkt
+  const [todos, setTodos] = useState<Todo[]>([]);
+  useEffect( () =>{
+    (async () => {
+      const response = await axios.get("/api/todo");
+      setTodos(response.data);
+    }) ()
+  }, []);
 
+    // post endpunkt
+    const initialTodo={
+        description : "",
+        status : "OPEN"
+    }
+
+    const [newTodo, setNewTodo] = useState<postTodo>(initialTodo);
+    const submit = () => {
+        (async () => {
+            const response = await axios.post("/api/todo", newTodo);
+            setTodos([...todos,response.data]);
+        })();
+    }
+
+    const changeDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const nameIsDescription = event.target.name; // aber nur in diesem fall da wir ein input haben ist immer das gleiche input ist
+        const value = event.target.value;
+        setNewTodo({ ...newTodo, [nameIsDescription] : value} ) // computed property's
+    }
+
+        // delete by ID endpunkt
+    const [idToDelete, setIdToDelete] = useState<string>("");
+    console.log("die Id die gelöscht werden soll" ,idToDelete)
+    const submit2 = () => {
+        (async () => {
+            const response = await axios.delete("/api/todo/"+ idToDelete);
+            const todosMap = todos.filter(todo => todo.id != idToDelete).map(todo => todo);
+            setTodos(todosMap);
+            console.log("hier ",todosMap);
+        })();
+
+    }
+    console.log("Hier gibts die INFOOOO ",todos);
+    const typeId = (event : React.ChangeEvent<HTMLInputElement>) => {
+        //const name = event.target.name;
+        const value = event.target.value;
+        setIdToDelete(value);
+    }
+
+  return (
+      <div >
+        <h1>todolist</h1>
+        {todos.map(todo => <li key={todo.id}> {todo.description} <button onClick={()=>{ }}> Weiter </button></li> )}
+
+          <form onSubmit={submit} >
+              <input type="text" name="description" value={newTodo.description} onChange={changeDescription}/>
+              <button> description BESTÄTIGEN  </button>
+          </form>
+
+          <form onSubmit={submit2}  >
+              <input type="text" name="delete" value={idToDelete} onChange={typeId}/>
+              <button type={"reset"} onClick={()=>submit2()}> Delete By ID</button>
+          </form>
+      </div>
+  )
+}
 export default App;
